67ea5ebf0180538866be4c399adc4854fcb777a9
Julian Heinovski <julian93@mail.uni-paderborn.de>
2015-08-21 16:10:35 +0200

    Move all listings as methods in the main.cpp. This is much easier.

---
+++ src/listings/002/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 21.08.2015
+ *      Author: Julian
+ */
+
+#include <iostream>
+using namespace std;
+
+void listing001() {
+	cout << "Am I a nerd?" << endl;
+}
+
+void listing002() {
+	cout << "I like programming\n";
+	cout << "A monster with "
+		 << "two rows\n";
+}
+
+void listing003() {
+	int ival = 0; // in C++11 better: int ival(0);
+	cout << "Please type in a natural number: ";
+	if (cin >> ival)
+		cout << "You entered the number " << ival << "\n";
+	else
+		cerr << "Error while reading the input!\n";
+}
+
+int main () {
+	//listing001();
+	//listing002();
+	listing003();
+
+	return 0;
+}
